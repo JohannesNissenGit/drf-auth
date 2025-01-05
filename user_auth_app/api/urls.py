d948cdffa58a2d4f83597f8aa432eb011e4cf042
NiJo89 <j.nissen@right-basedonscience.de>
2025-01-05 18:55:23 +0100

    :sparkles: token authentication: login view ( CustomLoginView )

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import UserProfileList, UserProfileDetail, RegistrationView
+from .views import UserProfileList, UserProfileDetail, RegistrationView, CustomLoginView
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
     path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
     path('registration/', RegistrationView.as_view(), name='registration'),
-    path('login/', obtain_auth_token, name='login')
+    path('login/', CustomLoginView.as_view(), name='login')
+    # path('login/', obtain_auth_token, name='login') # not necessary if using CustomLoginView
 ]
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -26,14 +27,35 @@ class RegistrationView(APIView):
         data = {}
         if serializer.is_valid():
             saved_account = serializer.save()
-            token, created = Token.objects.get_or_create(       # ',created' needed to access the token tuple
-                user=saved_account)                             # fetch existing token or create a token for the user
+            token, created = Token.objects.get_or_create(  # ',created' needed to access the token tuple
+                user=saved_account)  # fetch existing token or create a token for the user
             data = {
                 'token': token.key,
                 'username': saved_account.username,
                 'email': saved_account.email
             }
             return Response(data, status=status.HTTP_201_CREATED)
+
+        data = serializer.errors
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomLoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]  # allow any user to register
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(  # ',created' needed to access the token tuple
+                user=user)  # fetch existing token or create a token for the user
+            data = {
+                'token': token.key,
+                'username': user.username,
+                'email': user.email
+            }
+            return Response(data, status=status.HTTP_200_OK)
 
         data = serializer.errors
         return Response(data, status=status.HTTP_400_BAD_REQUEST)
