6741c41de0c65e39165a930b687e32825ae117dd
NiJo89 <j.nissen@right-basedonscience.de>
2025-01-05 18:44:17 +0100

    :sparkles: token authentication: registration view; add new users via new registration

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -1,11 +1,39 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from user_auth_app.models import UserProfile
-from .serializers import UserProfileSerializer
+from .serializers import UserProfileSerializer, RegistrationSerializer
+
 
 class UserProfileList(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
+
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+
+class RegistrationView(APIView):
+    permission_classes = [AllowAny]  # allow any user to register
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            saved_account = serializer.save()
+            token, created = Token.objects.get_or_create(       # ',created' needed to access the token tuple
+                user=saved_account)                             # fetch existing token or create a token for the user
+            data = {
+                'token': token.key,
+                'username': saved_account.username,
+                'email': saved_account.email
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+
+        data = serializer.errors
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
